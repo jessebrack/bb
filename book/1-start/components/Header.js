137dc4ea1fd46c630343351fc6c2f4582f3fcc33
Jesse Brack <jtbrack@gmail.com>
2018-10-09 10:31:04 -0400

    Server setup

--- book/1-start/components/Header.js
+++ book/1-start/components/Header.js
@@ -1,20 +1,63 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import Hidden from '@material-ui/core/Hidden';
 import { styleToolbar } from './SharedStyles';
 
-const Header = () => (
-  <div>
-    <Toolbar style={styleToolbar}>
-      <Grid container direction="row" justify="space-around" align="center">
-        <Grid item xs={12} style={{ textAlign: 'right' }}>
-          <Link prefetch href="/login">
-            <a style={{ margin: '0 20px 0 auto' }}>Log in</a>
-          </Link>
+function Header({ user }) {
+  return (
+    <div>
+      <Toolbar style={styleToolbar}>
+        <Grid container direction="row" justify="space-around" alignItems="center">
+          <Grid item sm={10} xs={9} style={{ textAlign: 'left' }}>
+            {user ? (
+              <Hidden smDown>
+                <Link prefetch href="/">
+                  <a style={{ marginRight: '20px' }}>Settings</a>
+                </Link>
+              </Hidden>
+            ) : (
+              <Link prefetch href="/">
+                <Avatar
+                  src="https://storage.googleapis.com/builderbook/logo.svg"
+                  alt="Builder Book Logo"
+                  style={{ margin: '0 auto 0 20px' }}
+                />
+              </Link>
+            )}
+          </Grid>
+          <Grid item sm={1} xs={3} style={{ textAlign: 'right' }}>
+            {user ? (
+              <div style={{ whiteSpace: 'nowrap' }}>
+                <Avatar
+                  src={user.avatarUrl}
+                  alt={user.displayName}
+                  style={{ margin: '0 auto 0 20px' }}
+                />
+              </div>
+            ) : (
+              <Link prefetch href="/login">
+                <a style={{ margin: '0 20px 0 auto' }}>Log in</a>
+              </Link>
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-    </Toolbar>
-  </div>
-);
+      </Toolbar>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  user: PropTypes.shape({
+    avatarUrl: PropTypes.string,
+    displayName: PropTypes.string,
+  }),
+};
+
+Header.defaultProps = {
+  user: null,
+};
 
 export default Header;
--- book/1-start/pages/index.js
+++ book/1-start/pages/index.js
@@ -1,16 +1,33 @@
 import Head from 'next/head';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
       <meta name="description" content="This is the description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>
+      Email:
+      {user.email}
+    </p>
     <Button variant="raised">MUI Button</Button>
   </div>
 );
+
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);

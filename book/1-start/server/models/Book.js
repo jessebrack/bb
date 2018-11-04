906d5cabbfe150d7c5df459b693d7788671b4e43
Jesse Brack <jtbrack@gmail.com>
2018-11-03 23:20:19 -0400

    Book model paramters

--- book/1-start/server/models/Book.js
+++ book/1-start/server/models/Book.js
@@ -3,7 +3,28 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const mongoSchema = new Schema({
-  // parameters
+  name: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  githubRepo: {
+    type: String,
+    required: true,
+  },
+  githubLastCommitSha: String.
 });
 
 class BookClass {
